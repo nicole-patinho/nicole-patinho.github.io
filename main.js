3281cd750614e95908f5945b80490ae5fd18b2a9
Nicole Patiño <150207905+nicole-patinho@users.noreply.github.com>
2023-11-19 15:11:50 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ let app = document.getElementById('typewriter');
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
- cursor: "<span style='color: #fb4993;'>|</span>",
+ cursor: "<span style='color: #4C2A85;'>|</span>",
 });
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #F7A9A8;">Soy estudiante de Ingeniería Mecatrónica.</span>')
+  .typeString('<span style="color: #320E3B;">Soy estudiante de Ingeniería Mecatrónica.</span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
